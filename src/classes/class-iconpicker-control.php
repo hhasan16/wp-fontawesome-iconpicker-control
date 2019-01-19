a9c31d94e80c4d8cb115e3b66cfbb475ce8caf5d
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2019-01-19 13:36:42 -0200

    Add CSS

--- src/classes/class-iconpicker-control.php
+++ src/classes/class-iconpicker-control.php
@@ -31,10 +31,10 @@ if ( class_exists( 'WP_Customize_Control' ) && ! class_exists( 'ThanksToIT\WPFAI
 				<span class="customize-control-title">
 					<?php echo esc_html( $this->label ); ?>
 				</span>
-                <div class="input-group icp-container">
-                    <input data-placement="bottomRight" class="wpfaipc icp-auto" <?php $this->link(); ?>
+                <div class="wpfaipc-wrapper input-group icp-container">
+                    <input data-placement="top" class="wpfaipc icp-auto" <?php $this->link(); ?>
                            value="<?php echo esc_attr( $this->value() ); ?>" type="text">
-                    <span class="input-group-addon"></span>
+                    <span class="wpfaipc-icon input-group-addon"></span>
                 </div>
             </label>
 			<?php
@@ -48,6 +48,7 @@ if ( class_exists( 'WP_Customize_Control' ) && ! class_exists( 'ThanksToIT\WPFAI
 			$font_awesome_url="//use.fontawesome.com/releases/v5.5.0/css/all.css";
 			wp_enqueue_script( 'wpfaipc-fontawesome-iconpicker', $itsjavi_url . '/js/fontawesome-iconpicker.min.js', array( 'jquery' ), '1.0.0', true );
 			wp_enqueue_script( 'wpfaipc-iconpicker-control', $this->base_url . '/assets/js/wpfaipc-iconpicker-control.js', array( 'jquery' ), '1.0.0', true );
+			wp_enqueue_script( 'wpfaipc-iconpicker-control', $this->base_url . '/assets/css/wpfaipc-iconpicker-control.css', array( 'jquery' ), '1.0.0', true );
 			wp_enqueue_style( 'wpfaipc-fontawesome-iconpicker', $itsjavi_url . '/css/fontawesome-iconpicker.min.css' );
 			wp_enqueue_style( 'wpfaipc-fontawesome', $font_awesome_url );
 		}
